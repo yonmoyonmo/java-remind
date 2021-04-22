5e50310b26206d7462bfd0418c4d2f226deb9556
yonmoyonmo <yonmosound@gmail.com>
2021-04-23 00:48:08 +0900

    random three done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        RandomMaker randomMaker = new RandomMaker(99999);
+        System.out.println(randomMaker.getRandomThree().toString());
     }
 }
---
+++ src/com/company/RandomMaker.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RandomMaker {
+    private Integer seed;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RandomMaker that = (RandomMaker) o;
+        return seed.equals(that.seed);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seed);
+    }
+
+    public Integer getSeed() {
+        return seed;
+    }
+
+    public void setSeed(Integer seed) {
+        this.seed = seed;
+    }
+
+    public RandomMaker(Integer seed) {
+        this.seed = seed;
+    }
+    public RandomMaker() {this.seed = 0;}
+
+    //억지 스트림과 람다를 써 봄
+    public List<Integer> getRandomThree(){
+        List<Integer> result = Stream.iterate(new Random().nextInt(this.seed), x ->{
+            return new Random().nextInt(this.seed);
+        }).limit(3).collect(Collectors.toList());
+        return  result;
+    }
+}
